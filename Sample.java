bf5a31736df5cfa92f769aceea4818840efb78ed
mandapravallika <pravallika.manda06@gmail.com>
2022-11-09 16:34:30 +0530

    file sending

---
+++ Sample.java
@@ -0,0 +1,20 @@
+class Sample {
+	static String str="Welcome";
+	
+	public static void main(String [] args) {
+		System.out.println(str);
+		
+		String s1="abc";
+		char ch='c';
+		char[] charr={'d','e','f','g'};
+		
+		
+		System.out.println(s1);
+		System.out.println(ch);
+		System.out.println(charr);
+		
+
+
+}
+
+}
